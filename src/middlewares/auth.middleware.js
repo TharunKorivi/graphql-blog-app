a8668d50e33da4a95dfea70344f4c4996f2c01f3
TharunKorivi <tharun.korivi04@gmail.com>
2026-05-22 18:48:39 +0530

    Corrected token extraction

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,11 +2,9 @@ import { verifyAccessToken } from "../utils/jwt.js";
 
 export const authMiddleware = (req, res, next) => {
     try {
-        const token =
-            req.cookies?.accessToken ||
-            (req.headers.authorization?.startsWith("Bearer ")
-                ? req.headers.authorization.split(" ")[1]
-                : null);
+        const token = req.headers.authorization?.startsWith("Bearer ")
+            ? req.headers.authorization.split(" ")[1]
+            : null;
 
         if (!token) {
             req.userId = null;
